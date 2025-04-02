b5b997ec141a44ece8a0a78da9df85bfc93a1c6b
Spoked <dreu.lavelle@gmail.com>
2025-04-02 04:11:34 -0400

    fix: added ffprobe command to cli parser

--- RTN/cli.py
+++ RTN/cli.py
@@ -10,6 +10,10 @@ def main():
     parse_parser = subparsers.add_parser('parse', help='Parse a filename or directly parse a string')
     parse_parser.add_argument('filename', nargs='?', type=str, help='The filename to parse or a string to directly parse')
 
+    # FFprobe command
+    ffprobe_parser = subparsers.add_parser('ffprobe', help='Parse a video file')
+    ffprobe_parser.add_argument('filename', nargs='?', type=str, help='The video file to parse')
+
     args = parser.parse_args()
 
     if args.command is None:
@@ -21,6 +25,15 @@ def main():
         if args.filename:
             result = parse(args.filename)
             print(result.model_dump_json(indent=4, exclude_none=True, exclude_defaults=True))
+
+    if args.command == 'ffprobe':
+        from RTN import parse_media_file
+        if args.filename:
+            try:
+                result = parse_media_file(args.filename)
+                print(result.model_dump_json(indent=4, exclude_none=True, exclude_defaults=True))
+            except Exception as e:
+                print(f"Error Parsing File: {e}")
 
 
 if __name__ == "__main__":
